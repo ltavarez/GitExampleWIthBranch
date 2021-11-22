961b82ab0fffbfc64a520568e2050acb5274a487
Leonardo Tavárez <leonardotb.93@gmail.com>
2021-11-22 19:15:25 -0400

    add user message 2

--- GitExample/Form1.Designer.cs
+++ GitExample/Form1.Designer.cs
@@ -68,6 +68,7 @@ namespace GitExample
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
