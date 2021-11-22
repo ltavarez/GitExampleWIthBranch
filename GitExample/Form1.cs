3a5c4fbd7265ff1e0138c2fe0cb7754662dc9c13
Leonardo Tavárez <leonardotb.93@gmail.com>
2021-11-22 19:13:44 -0400

    Add hello message

--- GitExample/Form1.cs
+++ GitExample/Form1.cs
@@ -16,5 +16,10 @@ namespace GitExample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "hola";
+        }
     }
 }
